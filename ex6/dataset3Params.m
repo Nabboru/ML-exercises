00c852f85dd7efd4b69c3e73ffafe8b2b2b4e94f
Leticia Piucco <leticia.piucco@gmail.com>
2021-04-15 12:47:58 +0100

    Add exercise 6

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -8,8 +8,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %
 
 % You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
+C = 0.3;
+sigma = 0.1;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -22,10 +22,25 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+values = [ 0.01 0.03 0.1 0.3 1 3 10 30 ];
+min_error = Inf
 
 
-
-
+for i = 1:length(values)
+    for j = 1:length(values)
+        new_c = values(i)
+        new_sigma = values(j)
+        model= svmTrain(X, y, new_c, @(x1, x2) gaussianKernel(x1, x2, new_sigma)); 
+        predictions = svmPredict(model, Xval);
+        error = mean(double(predictions ~= yval))
+        
+        if error < min_error
+            min_error =  error
+            C = new_c
+            sigma = new_sigma
+        end
+    end
+end
 
 
 
